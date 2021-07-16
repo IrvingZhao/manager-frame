a599ef89d94f712bded4a64a686e823df2ce5546
irving <irving.zhao.dev@hotmail.com>
2021-07-16 14:27:34 +0800

    初始化配置

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import {defineConfig} from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const host = 'localhost'
+const port = 3000
+const baseUrl = '/'
+const outputDir = 'dist'
+
+const proxy = {
+  '/api': {
+    target: 'http://localhost:8080',
+    changeOrigin: true,
+    ws: true,
+    logLevel: 'debug'
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vueJsx({
+      babelPlugins: [
+        [
+          'component',
+          {
+            libraryName: 'element-plus',
+            styleLibraryName: '~theme/lib'
+          }
+        ]
+      ]
+    })
+  ],
+  base: baseUrl,
+  server: {
+    host,
+    port,
+    proxy,
+  },
+  build: {
+    outDir: outputDir
+  }
+})
